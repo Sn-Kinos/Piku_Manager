4233d6933ef59b5d2a7b93d361b5d7e925f1225d
Sn-Kinos <dony435@naver.com>
2020-02-25 03:34:23 +0900

    Initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    YT_API_KEY: "AIzaSyDMGT9A0rBdENv1UHH8-qlsVH7T3RMLB7o",
+    PIKU_ID: "3UawjY"
+};
---
+++ routes/index.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const fs = require('fs');
+const fetch = require('node-fetch');
+const config = require('../config');
+const request = require('request-promise-native');
+
+router.get('/', async function(req, res, next) {
+  const piku = await fs.readFileSync('./db/piku.json');
+  const pikuJson = JSON.parse(piku.toString());
+
+  res.render('index', {
+    title: 'PIKU MANAGER',
+    piku: pikuJson
+  });
+});
+
+router.post('/refresh', async (req, res) => {
+  const piku = await fs.readFileSync('./db/piku.json');
+  const pikuJson = JSON.parse(piku.toString());
+
+  for (let key in pikuJson) {
+    // if (!pikuJson[key]) {
+      // const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&key=${config.YT_API_KEY}&id=${key}`);
+      // const json = await response.json();
+      // pikuJson[key].title = json.items[0].snippet.title;
+      // await fs.writeFileSync('./db/piku.json', JSON.stringify(pikuJson));
+    // }
+    if (!pikuJson[key].added) {
+      // const add = await fetch(`http://www.piku.co.kr/w/make/video_upload.php`, {
+      //   method: 'POST',
+      //   body: {
+      //     u: config.PIKU_ID,
+      //     v: `https://www.youtube.com/watch?v=${key}`,
+      //     vs: 0,
+      //     ve: 0,
+      //   },
+      //   headers: {
+      //     cookie: 'PHPSESSID=4hcqt01pedtui54t5224kcfj52'
+      //   },
+      // });
+      //
+      // const json = await add.text();
+      // console.log(json)
+      const add = await request.post({
+        uri: `http://www.piku.co.kr/w/make/video_upload.php`,
+        method: 'post',
+        form: {
+          u: config.PIKU_ID,
+          v: `https://www.youtube.com/watch?v=${key}`,
+          vs: 0,
+          ve: 0,
+        },
+        headers: {
+          cookie: 'PHPSESSID=4hcqt01pedtui54t5224kcfj52'
+        },
+      });
+      console.log(add)
+    }
+
+    console.log(pikuJson[key])
+  }
+  res.redirect('/')
+});
+
+router.get('/:id', async (req, res) => {
+
+});
+
+module.exports = router;
