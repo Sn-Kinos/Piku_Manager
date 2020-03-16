31a1e5ffd1ace538217827f61857487418b30b8c
Sn Kinos <dony435@naver.com>
2020-03-16 11:47:44 +0900

    257

--- routes/index.js
+++ routes/index.js
@@ -24,9 +24,39 @@ router.get('/qurareforevermxforever', async function(req, res, next) {
   });
 });
 
-router.post('/refresh', async (req, res) => {
+router.post('/overwrite', async (req, res) => {
   const piku = await fs.readFileSync('./db/piku.json');
   const pikuJson = JSON.parse(piku.toString());
+
+  for (let i in pikuJson) {
+    const changeTitle = await request.post({
+      uri: "https://www.piku.co.kr/w/make/img_chg.php",
+      body: `uid=${config.PIKU_ID}&dno=${i}&iname=${pikuJson[i].title}`,
+      headers: {
+        cookie: config.PHPSESSID,
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/82.0.4056.0 Safari/537.36 Edg/82.0.432.3',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      }
+    })
+    console.log(changeTitle)
+    await request.post({
+      uri: 'https://www.piku.co.kr/w/make/video_upload.php',
+      method: 'post',
+      form: {
+        u: config.PIKU_ID,
+        v: `https://www.youtube.com/watch?v=${pikuJson[i].url}`,
+        vs: pikuJson[i].start || 0,
+        ve: pikuJson[i].end || 0,
+        ino: i,
+      },
+      headers: {
+        cookie: config.PHPSESSID,
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/82.0.4056.0 Safari/537.36 Edg/82.0.432.3',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      },
+    })
+    console.log(pikuJson[i], i)
+  }
 
   res.redirect('/')
 });
